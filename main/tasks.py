98f2e42b79181577842659f377f921cec70a1374
pbakiewicz <bakiewicz.p@gmail.com>
2019-07-02 22:40:40 +0200

    Modifying tasks for thumbnailing pics

--- main/tasks.py
+++ main/tasks.py
@@ -1,11 +1,17 @@
 from celery import shared_task
+from PIL import Image
+from .models import Picture
 
 
 @shared_task
-def modify_picture(*args, **kwargs):
-    pass
+def modify_picture(id_picture, *args, **kwargs):
+    try:
+        pic = Picture.objects.get(pk=id_picture)
+        image = Image.open(pic.image)
+        ext = pic.image.name.split(".")[-1]
+        image.thumbnail((20, 20))
 
+        image.save("img/" + pic.name + "_mini." + ext)
+    except Exception as e:
+        raise e
 
-@shared_task
-def reverse(string):
-    return string[::-1]
