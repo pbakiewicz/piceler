b94129e3be6e3b0cca4bb4732a8b325413989f43
pbakiewicz <bakiewicz.p@gmail.com>
2019-07-03 20:38:42 +0200

    serve media files with django

--- piceler/settings.py
+++ piceler/settings.py
@@ -16,6 +16,8 @@ import os
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
@@ -120,6 +122,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+MEDIA_URL = "/media/"
 
 CELERY_BROKER_URL = 'amqp://localhost'
 
